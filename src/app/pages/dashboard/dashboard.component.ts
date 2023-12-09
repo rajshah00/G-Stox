b92a031b9d307f18dc1ded9c1dd0f0528567469f
rajshah890 <rajshah.igen@gmail.com>
2023-12-09 13:01:52 +0530

    'servise123456'

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  stroge: any = JSON.parse(localStorage.getItem("isLoggedIn") || '')
+  constructor(public service: ApiServiceService) {
 
+  }
+
+  ngOnInit() {
+    this.service.getClintDas(this.stroge.username).subscribe((res: any) => {
+      console.log("res", res)
+    }, (err: any) => {
+      console.log("err", err)
+    })
+  }
 }
