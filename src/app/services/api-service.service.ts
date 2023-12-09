f8a36317006e8e8a8136a72756635977e59a6350
rajshah890 <rajshah.igen@gmail.com>
2023-12-09 12:01:07 +0530

    'service123'

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -9,7 +9,7 @@ import { environment } from '../../environments/environment';
 export class ApiServiceService {
 
   constructor(
-    private router: Router,
+    private router: Router, 
     private http: HttpClient,
   ) { }
 
@@ -24,4 +24,169 @@ export class ApiServiceService {
   getProfile(ClientCode: any) {
     return this.http.get<any>(`${environment.API_URL}/Reports/ClientProfile/Get?ClientCode=${ClientCode}`);
   }
+
+  getClintDas(ClientCode: any) {
+    return this.http.get<any>(`${environment.API_URL}/Common/ClientDashBoard/Get?ClientCode=${ClientCode}`);
+  }
+
+  getSegmentBalance(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/FinancialBalance/Post', {}, { headers });
+  }
+
+  getFinancialLedger(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/FinancialLedger/Post', {}, { headers });
+  }
+
+  getClientHolding(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/ClientHolding/Post', {}, { headers });
+  }
+
+  getDerivativeNetPosition(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/DerivativeNetPosition/Post', {}, { headers });
+  }
+
+  getEquityLongShort(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/EquityLongShort/Post', {}, { headers });
+  }
+
+  getClientMasterDetail(ClientCode: any) {
+    return this.http.get<any>(`${environment.API_URL}/Masters/ClientMasterDetail/Get?Code=${ClientCode}&ClientType=A`);
+  }
+
+  getEquityTradeBook(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/EquityTradeBook/Post', {}, { headers });
+  }
+
+  getDerivativeTradeBook(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/DerivativeTradeBook/Post', {}, { headers });
+  }
+
+  getEquityNetPositionDateRange(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/EquityNetPositionDateRange/Post', {}, { headers });
+  }
+
+  getContractNoteWeb(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/ContractNoteWeb/Post', {}, { headers });
+  }
+
+  getDocumentSign(FileID: any) {
+    return this.http.get<any>(`${environment.API_URL}/Services/DocumentSign/Get?DocumentID=${FileID}&DocumentType=CNOTE`);
+  }
+
+  getEquityBillDetail(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/EquityBillDetail/Post', {}, { headers });
+  }
+
+  getDerivativeBillDetail(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/DerivativeBillDetail/Post', {}, { headers });
+  }
+
+  getAPAmount(FileID: any) {
+    return this.http.get<any>(`${environment.API_URL}/Vouchers/APAmount/Get?AccountID=${FileID}&RequestType=ONREQUEST&ReleaseFlag=N`);
+  }
+
+  getBankDetail(FileID: any) {
+    return this.http.get<any>(`${environment.API_URL}/Masters/BankDetail/Get?Code=${FileID}&BankID=&BankType=D`);
+  }
+
+  getPaymentRequest(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Vouchers/PaymentRequest/Post', {}, { headers });
+  }
+
+  getPaymentReleaseRequest(FileID: any) {
+    return this.http.get<any>(`${environment.API_URL}/Vouchers/PaymentReleaseRequest/Get?RequestDate=${FileID}&Release=N`);
+  }
+
+  getClientBrokerage(ClientCode: any) {
+    return this.http.get<any>(`${environment.API_URL}/Common/ClientBrokerage/Get?ClientCode=${ClientCode}`);
+  }
+
+
+  getClientCustomInfo(ClientCode: any) {
+    return this.http.get<any>(`${environment.API_URL}/Utilities/ClientCustomInfo/Get?AccountID=${ClientCode}`);
+  }
+
+  getFIFONetPositionReport(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/FIFONetPositionReport/Post', {}, { headers });
+  }
+
+  getEquityPnLReportWeb(data: any) {
+    return this.http.get<any>(`${environment.API_URL}/Masters/EquityPnLReportWeb/Get?ClientCode=${data.ClientCode}&FromDate=${data.FromDate}&ToDate=${data.ToDate}&ReportType=${data.ReportType}&ExportFormat=${data.ExportFormat} `);
+  }
+
+  getIFSCBankData(data: any) {
+    return this.http.get<any>(`${environment.API_URL}/Masters/IFSCBankData/Get?micr=${data.micr}&ifsc=${data.ifsc}`);
+  }
+
+  getDerivativeNetPositionDateRange(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/DerivativeNetPositionDateRange/Post', {}, { headers });
+  }
+
+  geClientCustomInfo(data: any) {
+    return this.http.get<any>(`${environment.API_URL}/Utilities/ClientCustomInfo/Get?AccountID=${data.ClientCode}`);
+  }
+
+  geGetOrionCDSLDetail(data: any) {
+    return this.http.get<any>(`${environment.API_URL}/Masters/GetOrionCDSLDetail/Get?PANNo=${data.PANNo}`);
+  }
+
+  getEquityTransactionLedger(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/EquityTransactionLedger/Post', {}, { headers });
+  }
+
+  getWEBDebtorCreditorList(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/WEBDebtorCreditorList/Post', {}, { headers });
+  }
+
+  getTurnoverBrokerageWebReport(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    return this.http.post(environment.API_URL + '/Reports/TurnoverBrokerageWebReport/Post', {}, { headers });
+  }
+
+  getBrokerageSummaryReport(data: any) {
+    data.Grant_type = "password";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', });
+    let url1 = environment.API_URL + '/Reports/IntroBrokReport/Post';
+    let url2 = environment.API_URL + '/Reports/BrokerageSummaryReport/Post';
+    return this.http.post(url1, {}, { headers });
+  }
+
+  geFIFOHolding(data: any) {
+    return this.http.get<any>(`${environment.API_URL}/Utilities/FIFOHolding/Get?AccountID=${data.ClientCode}`);
+  }
 }
+
+
