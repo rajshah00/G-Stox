efbf62f09cf602700e1bc3ba9f22d24a93e4f09a
rajshah890 <rajshah.igen@gmail.com>
2023-12-14 13:48:09 +0530

    '12/14/2023'

--- src/app/pages/layout/side-bar/side-bar.component.ts
+++ src/app/pages/layout/side-bar/side-bar.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss']
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
 
 }
