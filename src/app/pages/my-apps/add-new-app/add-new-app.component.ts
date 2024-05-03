baaeb4848f2d5d760265a7a2307ff7cf053eda7d
rajshah <rajshah.igen@gmail.com>
2024-05-03 11:56:38 +0530

    all changes push

---
+++ src/app/pages/my-apps/add-new-app/add-new-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-app',
+  templateUrl: './add-new-app.component.html',
+  styleUrls: ['./add-new-app.component.scss']
+})
+export class AddNewAppComponent {
+
+}
---
+++ src/app/pages/my-apps/my-apps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-apps',
+  templateUrl: './my-apps.component.html',
+  styleUrls: ['./my-apps.component.scss']
+})
+export class MyAppsComponent {
+
+}
